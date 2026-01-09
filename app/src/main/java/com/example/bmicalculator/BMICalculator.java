1c0f2a9d2f2eca6cee4b81eac580e42095a29aa9
Felix-wainaina <nduatifelixwain@gmail.com>
2026-01-09 18:35:38 +0300

    Implemented full BMI classification logic (Underweight, Normal, Overweight, Obese)

--- app/src/main/java/com/example/bmicalculator/BMICalculator.java
+++ app/src/main/java/com/example/bmicalculator/BMICalculator.java
@@ -10,10 +10,22 @@ public class BMICalculator
             {
                 throw new IllegalArgumentException("Weight and/or height must be greater than 0");
             }
-            return weight * height * height;
+            return weight / (height * height);
         } catch (RuntimeException e)
         {
             throw new RuntimeException(e);
+        }
+    }
+    // Method to classify the BMI based on assignment ranges
+    public static String getCategory(float bmi) {
+        if (bmi < 18.5) {
+            return "Underweight";
+        } else if (bmi >= 18.5 && bmi < 25) {
+            return "Normal";
+        } else if (bmi >= 25 && bmi < 30) {
+            return "Overweight";
+        } else {
+            return "Obese";
         }
     }
 }
