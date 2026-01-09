bf9d37d64be5636875a349ee3660aa6654bdd687
Nevean <nevadhis@gmail.com>
2026-01-09 22:45:26 +0300

    Added validation logic to check for empty input fields.

--- app/src/main/java/com/example/bmicalculator/MainActivity.java
+++ app/src/main/java/com/example/bmicalculator/MainActivity.java
@@ -4,24 +4,26 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import android.view.Window;
-import android.widget.Button;
-import android.widget.EditText;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowCompat;
 import androidx.core.view.WindowInsetsCompat;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.button.MaterialButton;
 
 public class  MainActivity extends AppCompatActivity {
-    EditText etWeight;
-    EditText etHeight;
-    Button btnCalculate;
+    TextInputEditText etWeight;
+    TextInputEditText etHeight;
+    MaterialButton btnCalculate;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
 //        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
@@ -40,4 +42,15 @@ public class  MainActivity extends AppCompatActivity {
         float bmi = BMICalculator.calculateBMI(1.1f, 2.2f);
         */
     }
+    private boolean validateInput() {
+        return etWeight.getText() != null &&
+                etHeight.getText() != null &&
+                !etWeight.getText().toString().trim().isEmpty() &&
+                !etHeight.getText().toString().trim().isEmpty();
+    }
+
+
+
+
+
 }
